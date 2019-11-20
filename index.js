02b8d2bfbb88b20b44788d5519cf34cd71b1ae05
Slavo Kozar <slavo.kozar@gmail.com>
2019-11-20 23:34:32 +0000

    updated to v 1.0.5

--- index.js
+++ index.js
@@ -19,6 +19,24 @@ function mySwitch(log, config) {
 
 mySwitch.prototype = {
 
+getServices: function () {
+    let informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
+      .setCharacteristic(Characteristic.Model, "My switch model")
+      .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
+
+    let switchService = new Service.Switch("My switch");
+    switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getSwitchOnCharacteristic.bind(this))
+      .on('set', this.setSwitchOnCharacteristic.bind(this));
+
+    this.informationService = informationService;
+    this.switchService = switchService;
+    return [informationService, switchService];
+  }
+
   getSwitchOnCharacteristic: function (next) {
     const me = this;
     request({

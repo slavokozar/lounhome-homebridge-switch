4b2b17574542a0d06788c757e1e1d037155e411a
Slavo Kozar <slavo.kozar@gmail.com>
2019-11-20 23:05:05 +0000

    test

---
+++ index.js
@@ -0,0 +1,55 @@
+const request = require('request');
+const url = require('url');
+
+
+const Service, Characteristic;
+
+module.exports = function (homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("lounhome-homebridge-switch", "LounHome-HomeBridge-Switch", mySwitch);
+};
+
+
+function mySwitch(log, config) {
+  this.log = log;
+  this.getUrl = config['getUrl'];
+  this.postUrl = config['postUrl'];
+}
+
+mySwitch.prototype = {
+
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+      url: me.getUrl,
+      method: 'GET',
+    },
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.status);
+    });
+  },
+
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'value': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
